ce552b8d0f997b3793a4c827b279a51b7f30b74e
Subhash Chandran <subwiz@gmail.com>
2016-01-04 21:43:09 +0530

    adding support to Windows. Minor improvements.

--- sdk-update/src/dist/bin/frsh.bat
+++ sdk-update/src/dist/bin/frsh.bat
@@ -5,13 +5,13 @@ IF NOT DEFINED FRSH_HOME (
 )
 
 IF EXIST %FRSH_HOME%\config\first_install.bat (
-  call %FRSH_HOME%\config\first_install.bat
+  CALL %FRSH_HOME%\config\first_install.bat
   REM mklink /D %FRSH_HOME%\sdk\latest %FRSH_HOME%\sdk\frshsdk-%version%
-  DEL %FRSH_HOME%\config\first_install.bat
+  REM DEL %FRSH_HOME%\config\first_install.bat
 )
 
 IF NOT EXIST %FRSH_HOME%\sdk\latest\bin\frsh.bat (
-  echo Installation not complete. Please run 'frsh-update.bat'.
+  ECHO Installation not complete. Please run 'frsh-update.bat'.
+) else (
+  %FRSH_HOME%\sdk\latest\bin\frsh.bat %*
 )
-
-%FRSH_HOME%\sdk\latest\bin\frsh.bat %*
