ef17315bfa5ec2be1b84c24909e71bd545edd5cc
Subhash Chandran <subwiz@gmail.com>
2016-01-05 15:34:11 +0530

    Updating tested bat script.

--- sdk-update/src/dist/bin/frsh.bat
+++ sdk-update/src/dist/bin/frsh.bat
@@ -2,16 +2,21 @@
 
 IF NOT DEFINED FRSH_HOME (
   ECHO Please set the FRSH_HOME environment variable.
+  EXIT /B
 )
 
-IF EXIST %FRSH_HOME%\config\first_install.bat (
-  CALL %FRSH_HOME%\config\first_install.bat
-  REM mklink /D %FRSH_HOME%\sdk\latest %FRSH_HOME%\sdk\frshsdk-%version%
-  REM DEL %FRSH_HOME%\config\first_install.bat
+IF EXIST "%FRSH_HOME%\config\first_install.bat" (
+  CALL "%FRSH_HOME%\config\first_install.bat"
+  IF DEFINED version (
+    mklink /D "%FRSH_HOME%\sdk\latest" "%FRSH_HOME%\sdk\frsh-%version%"
+    IF EXIST "%FRSH_HOME%\sdk\latest" (
+      DEL "%FRSH_HOME%\config\first_install.bat"
+    )
+  )
 )
 
-IF NOT EXIST %FRSH_HOME%\sdk\latest\bin\frsh.bat (
+IF NOT EXIST "%FRSH_HOME%\sdk\latest\bin\frsh.bat" (
   ECHO Installation not complete. Please run 'frsh-update.bat'.
 ) else (
-  %FRSH_HOME%\sdk\latest\bin\frsh.bat %*
+  "%FRSH_HOME%\sdk\latest\bin\frsh.bat" %*
 )
