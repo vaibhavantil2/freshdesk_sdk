6ce9010a9094a9d900ec5debf6e5df1a4ee2d344
Arun <arun@freshdesk.com>
2016-02-03 13:01:00 +0530

    plug execution logic changed for app.js

--- sdk/src/main/java/com/freshdesk/sdk/plug/PlugResponse.java
+++ sdk/src/main/java/com/freshdesk/sdk/plug/PlugResponse.java
@@ -63,7 +63,7 @@ public class PlugResponse {
         StringBuilder sb = new StringBuilder();
         final String jsContents = getFileContent(jsFile);
         sb.append("<script type='text/javascript'>\n")
-                .append("Freshapp.run(function() {"+  jsContents +"});\n")
+                .append("Freshapp.run(function() { \n (function() { \n " +  jsContents +"}().init()); });\n")
                 .append("</script>");
         return sb.toString();
     }
