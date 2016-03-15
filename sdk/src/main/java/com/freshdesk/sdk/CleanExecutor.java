f477f19c72338b23ea05697cfe4660357a549bd5
raghu <raghu12133@gmail.com>
2016-03-15 15:50:49 +0530

    frsh clean fixed - https://github.com/freshdesk/freshapps_devportal/issues/807

--- sdk/src/main/java/com/freshdesk/sdk/CleanExecutor.java
+++ sdk/src/main/java/com/freshdesk/sdk/CleanExecutor.java
@@ -2,11 +2,13 @@ package com.freshdesk.sdk;
 
 import io.airlift.airline.Command;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.zeroturnaround.zip.commons.FileUtils;
 
 /**
  *
@@ -29,25 +31,14 @@ public class CleanExecutor extends AbstractProjectExecutor {
         String errorMsg = null;
 
         for (String cleanableDir : CLEANABLE_DIRS) {
-            File dir = new File(prjDir, cleanableDir);
-            if (dir.exists() && dir.isDirectory()) {
-                final File[] files = dir.listFiles();
-                if (files != null) {
-                    for (final File f : files) {
-                        boolean ret = f.delete();
-                        if (!ret) {
-                            errorMsg = "Unable to delete file: " + f.getName();
-                            exitStatus = ExitStatus.CMD_FAILED;
-                        }
-                    }
+            try {
+                FileUtils.deleteDirectory(new File(prjDir, cleanableDir));
+                if (verbose) {
+                    System.out.println("Removed: " + cleanableDir + "/");
                 }
-                boolean ret = dir.delete();
-                if (!ret) {
-                    errorMsg = "Unable to delete dir: " + dir.getName();
-                    exitStatus = ExitStatus.CMD_FAILED;
-                } else if (verbose) {
-                    System.out.println("Removed: " + dir.getName() + "/");
-                }
+            }
+            catch (IOException e) {
+                throw new SdkException(ExitStatus.CMD_FAILED, "Unable to delete dir: " + cleanableDir);
             }
         }
         
