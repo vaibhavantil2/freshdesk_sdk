290f85d1e24df087ca3fa46edceba11a518b9dbc
Subhash Chandran <subwiz@gmail.com>
2016-03-21 12:54:00 +0530

    modified to stream implementation.

--- sdk/src/main/java/com/freshdesk/sdk/DigestUtil.java
+++ sdk/src/main/java/com/freshdesk/sdk/DigestUtil.java
@@ -1,66 +1,59 @@
 package com.freshdesk.sdk;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
-import org.wiztools.commons.FileUtil;
+import org.wiztools.commons.HexEncodeUtil;
 
 /**
  *
  * @author raghav
  */
 public class DigestUtil {
-
-    private static String getHashCode(InputStream is) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            String content = getStringFromInputStream(is);
-            byte[] array = md.digest(content.getBytes()); 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException ex) {
-            throw new SdkException(ExitStatus.CMD_FAILED, "Unable to generate hash.");
-        }
-    }
-
+    
     public static String getHashCodeForFiles(List<File> files, ManifestContents manifest) {
-        StringBuilder content = new StringBuilder();
-        try {
-            for (File f : files) {
-                content.append(FileUtil.getContentAsString(f, manifest.getCharset()));
+        DigestCompute dc = new DigestCompute();
+        for (File f : files) {
+            try(
+                    RandomAccessFile aFile = new RandomAccessFile(f, "r");
+                    FileChannel inChannel = aFile.getChannel();) {
+                MappedByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+                buffer.load();
+                dc.update(buffer);
+                buffer.clear();
             }
-            return getHashCode(new ByteArrayInputStream(content.toString().getBytes()));
+            catch (IOException ex) {
+                throw new SdkException(ExitStatus.CMD_FAILED, "Error while generating Digest.");
+            }   
         }
-        catch (IOException ex) {
-            throw new SdkException(ExitStatus.CMD_FAILED, "Error while generating Digest.");
-        }
+        return dc.getHexHash();
     }
     
-    private static String getStringFromInputStream(InputStream is) {
-        try {
-            StringBuilder out =new StringBuilder();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String newLine = System.getProperty("line.separator");
-            String line;
-            while((line = br.readLine()) != null) {
-                out.append(line);
-                out.append(newLine);
+    static class DigestCompute {
+        
+        private final MessageDigest md;
+        
+        DigestCompute() {
+            try {
+                md = MessageDigest.getInstance("MD5");
             }
-            // remove the last newline element.
-            out.deleteCharAt(out.toString().length() - 1);
-            return out.toString();
-        } catch (IOException ex) {
-            throw new SdkException(ExitStatus.CMD_FAILED, "Error while reading content from file.");
+            catch(NoSuchAlgorithmException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        
+        void update(ByteBuffer bb) throws IOException {
+            md.update(bb);
+        }
+        
+        String getHexHash() {
+            return HexEncodeUtil.bytesToHex(md.digest());
         }
     }
-
 }
