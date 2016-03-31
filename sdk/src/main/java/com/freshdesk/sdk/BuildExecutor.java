69aa49d7e1ffa4062d576e6bf411cf148716a774
Subhash Chandran <subwiz@gmail.com>
2016-03-31 16:33:00 +0530

    code cleanup...

--- sdk/src/main/java/com/freshdesk/sdk/BuildExecutor.java
+++ sdk/src/main/java/com/freshdesk/sdk/BuildExecutor.java
@@ -12,18 +12,11 @@ public class BuildExecutor extends AbstractProjectExecutor {
 
     @Override
     public void execute() throws SdkException {
-      try {
         BuildFileHandler bfh = new BuildFileHandler(prjDir, manifest);
         bfh.generateBuildFile();
         if(verbose) {
-          System.out.println("Generated build file.");
+            System.out.println("Generated build file: build/index.html.");
         }
-      }
-      catch(SdkException ex) {
-        if(verbose) {
-          System.out.println("Error while generating build file.");
-        }
-      }
     }
-    
+
 }
