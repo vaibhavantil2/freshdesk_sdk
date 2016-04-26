c0307719a0c76e265c9ed4568f5ec0a75ded233d
Raghav <raghav@freshdesk.com>
2016-04-27 02:13:12 +0530

    Cli parsing lib changed
    1. Fixing commands that need to be executed only from project directory.
    2. Cli parsing replaced to wiz-cliparse from commander

--- index.js
+++ index.js
@@ -2,81 +2,94 @@
 
 "use strict";
 
-var program = require('commander');
+var Program = require('wiz-cliparse');
 
 var pjson = require(__dirname + '/package.json');
 global.pjson = pjson;
 var validationConst = require( __dirname + '/lib/constants').validationContants;
 
-// Cli parsing:
-program.version(pjson.version)
-  .option('-v, --verbose', 'verbose execution', function() {
+// Cli Parsing:
+// Fetching arguments from cli
+var cliArg = process.argv.slice(2);
+
+// Registering Cli commands
+var prg = new Program('frsh', '[global-options] [command] [command-options] [arguments]');
+prg.addHelp();
+
+prg.addOpt('v', 'verbose', 'enable verbose output.');
+prg.addOpt('x', 'exception', 'display exception trace');
+
+var initCmd = prg.addCmd('init', 'create a new project.');
+initCmd.addOpt('h', 'help', 'Usage: \n frsh [global-options] init <type> [dir]\n type can be "plug"');
+prg.addCmd('info', 'display information about the project.');
+var runCmd = prg.addCmd('run', 'local testing.');
+runCmd.addOpt('h', 'help')
+prg.addCmd('validate', 'run all validations.');
+prg.addCmd('pack', 'pack for distribution.');
+prg.addCmd('clean', 'cleans build/ and dist/ dirs.');
+prg.addCmd('version', 'prints the version of SDK.');
+
+// Pasring for commands
+var res = prg.parseSync(cliArg);
+
+// Setting the global options
+if(res.gopts) {
+  if(res.gopts.has('v')) {
     global.verbose = true;
-    return true;
-  })
-  .option('-x, --exception', 'print exception trace', function(){
+  }
+  if(res.gopts.has('x')) {
     global.trace = true;
-    return true;
-  });
+  }
+}
 
-program.command('init <type> [dir]')
-  .description('create a new project.')
-  .action(function(type, dir){
-    require(__dirname + '/lib/cli-init').run(type, dir);
-  });
+// Print and exit if command is help
+if(res.gopts.has('h') || res.cmd === 'help') {
+  prg.printHelp(res);
+}
+// Other commands.
+switch(res.cmd) {
 
-program.command('info')
-  .description('display information about the project.')
-  .action(function(){
+  case 'init':
+    if(res.opts.has('h')) {
+      console.log("Usage:\nfrsh init <type> [dir]\n  <type> is plug\n  " +
+       "[dir] (optional) - dir to init the project, initializes in current working dir if no option is specified");
+      break;
+    }
+    require(__dirname + '/lib/cli-init').run(res.args[0], res.args[1]);
+    break;
+
+  case 'info':
     require(__dirname + '/lib/cli-info').run();
-  });
+    break;
 
-program.command('run')
-  .description('local testing.')
-  .action(function(){
+  case 'run':
     require(__dirname + '/lib/cli-run').run();
-  });
+    break;
 
-program.command('validate').description('run all validations.')
-    .action(function(){
-      var validationStatus = require(__dirname + '/lib/cli-validate').run(validationConst.PRE_PKG_VALIDATION);
-      if(validationStatus) {
-        if(global.verbose) {
-          console.log("No failures observed when running validations.");
-        }
+  case 'validate':
+    var validationStatus = require(__dirname + '/lib/cli-validate').run(validationConst.PRE_PKG_VALIDATION);
+    if(validationStatus) {
+      if(global.verbose) {
+        console.log("No failures observed when running validations.");
       }
-      else {
-        console.log("Validation failed with above errors.");
-      }
-    });
+    }
+    else {
+      console.log("Validation failed with above errors.");
+    }
+    break;
 
-program.command('pack')
-  .description('pack for distribution.')
-  .action(function(){
+  case 'pack':
     require(__dirname + '/lib/cli-pack').run();
-  });
+    break;
 
-program.command('help', null, {isDefault:true})
-  .description('display help information.')
-  .action(function(env, options){
-    program.outputHelp();
-  });
-
-program.command('clean')
-  .description('cleans build/ and dist/ dirs.')
-  .action(function() {
+  case 'clean':
     require(__dirname + '/lib/cli-clean').run();
-  });
+    break;
 
-program.command('*', null, {noHelp:true})
-  .action(function(cmd){
-    console.error(`Unrecognized command: ${cmd}.`);
-    program.outputHelp();
-  });
+  case 'version':
+    console.log(pjson.version);
+    break;
 
-program.parse(process.argv);
-
-// Process no command execution:
-if (!process.argv.slice(2).length) {
-  program.outputHelp();
+  default:
+    prg.printHelp(res);
 }
