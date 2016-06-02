c3c438647b928e253c8664e0e7272d22450a0be0
Raghav <raghav@freshdesk.com>
2016-06-02 12:53:24 +0530

    adding copyright info to files, deleting charset from plug template and minor changes

--- test/test_liquid.js
+++ test/test_liquid.js
@@ -1,3 +1,6 @@
+/* Copyright (C) 2016 Freshdesk, Inc.
+This source code is a part of the Fresh SDK and is covered by the our license terms. For details about this license, please read the LICENSE.txt which is bundled with this source code. */
+
 var liquid = require(__dirname + '/../lib/liquid');
 var expect = require('chai').expect;
 
