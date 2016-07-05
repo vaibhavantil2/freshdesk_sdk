e2557a30bda37a637a60cd7f3fdc12186bbf7d35
raghu <raghu12133@gmail.com>
2016-07-05 18:22:03 +0530

    Template changes

--- template/default/app/app.js
+++ template/default/app/app.js
@@ -2,6 +2,25 @@
   return {
     initialize: function() {
       console.log("My first app!");
+      if(page_type == "ticket") {
+        var requesterName = domHelper.ticket.getTicketInfo().helpdesk_ticket.requester_name;
+        jQuery('#apptext').text("Ticket created by " + requesterName);
+      }
+      else if(page_type == "contact"){
+        var agentName = domHelper.contact.getContactInfo().user.name;
+        jQuery('#apptext').text("Hello " + agentName);
+      }
     }
   }
 })();
+
+{%comment%}
+
+# Iparam
+  The ​installation parameters or iparams ​are essentially the settings that you
+  want your users to configure when installing the apps.
+# Iparam Usage
+  - {{iparam.username}}
+  - {{iparam.country}}
+
+{%endcomment%}
