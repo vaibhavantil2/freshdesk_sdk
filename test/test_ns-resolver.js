8a0e3a23ef89cd9fce340cdd4e75f9f67dc6704c
raghu <raghu12133@gmail.com>
2016-07-26 09:41:27 +0530

    Renamed page-util to ns-resolver. Fixed ns-resolver test.

--- test/test_ns-resolver.js
+++ test/test_ns-resolver.js
@@ -11,14 +11,14 @@ bundled with this source code.
 var helper = require('./helper');
 var expect = require('chai').expect;
 
-describe('page util test', function(){
+describe('Name space resolver test', function(){
   before(function(){
     process.chdir(projectDir['name']);
   });
 
   it('should get app_id', function(done) {
     var ns = require(__dirname + '/../lib/utils/ns-resolver');
-    expect('{"app_id":"fa_fresha_101"}').eql(JSON.stringify(ns.getNamespace()));
+    expect('{"app_id":"fresha_101_101"}').eql(JSON.stringify(ns.getNamespace()));
     done();
   });
 });
