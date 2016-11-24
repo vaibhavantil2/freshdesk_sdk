894b66b8b6a4dac0af3edd34158da63c1c461925
raghu <raghu12133@gmail.com>
2016-11-24 14:54:38 +0530

    License header

--- test/test_run.js
+++ test/test_run.js
@@ -1,3 +1,13 @@
+/*
+
+Copyright (C) 2016 Freshdesk, Inc.
+
+This source code is a part of the Freshdesk SDK and is covered by the our license
+terms. For details about this license, please read the LICENSE.txt which is
+bundled with this source code.
+
+*/
+
 var request = require('supertest');
 var helper = require('./helper');
 var pjson = require('../package.json');
